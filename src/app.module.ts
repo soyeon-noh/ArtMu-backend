066930c4de5d33792507e59bcadc00b26c76d95a
soyeon-noh <1010qwerty12@gmail.com>
2022-03-06 20:05:06 +0900

    twiiter API connection success

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TwitterConfig } from './configs/TwitterConfig';
 import { DesignerModule } from './designer/designer.module';
+import { TweetModule } from './tweet/tweet.module';
 
 @Module({
-  imports: [DesignerModule],
+  imports: [ConfigModule.forRoot({}), DesignerModule, TweetModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TwitterConfig],
 })
 export class AppModule {}
